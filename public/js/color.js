0b880b8c085b28cc568b9219ec115327de3c764a
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-10 21:51:25 -0700

    put h1 in a header, changed paragraph to li, still not showing detail info

--- public/js/color.js
+++ public/js/color.js
@@ -1,16 +1,17 @@
 const root = document.getElementById('root');
-const p = document.createElement('p');
+const colorInfo = document.createElement('ul');
 
 const search = new URLSearchParams(window.location.search);
 const name = search.get('name');
 
 fetch(`/api/v1/colors/${name}`)
   .then(res => res.json())
-  .then(color => {
-    p.textContent = `${color.name}`;
+  .then(colors => {
+    colors.forEach(color => {
+      const li = document.createElement('li');
+      li.textContent = `${color.name}`;
+      colorInfo.appendChild(li);
+    });
   });
 
-root.appendChild(p);
-
-
-
+root.appendChild(colorInfo);
