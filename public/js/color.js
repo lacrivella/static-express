6320489ebbcc9db9c7c8c0b29a5fe36181e08873
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-11 09:51:03 -0700

    fixed h2 and h3s for color details

--- public/js/color.js
+++ public/js/color.js
@@ -2,14 +2,15 @@ const search = new URLSearchParams(window.location.search);
 const name = search.get('name');
 
 const root = document.getElementById('root');
-const colorInfo = document.createElement('h2');
+const h2 = document.createElement('h2');
 const h3 = document.createElement('h3');
 
 fetch(`/api/v1/colors/${name}`)
   .then(res => res.json())
   .then(color => {
-    h3.textContent = `${color.name} - ${color.hex} - ${color.r} - ${color.g} - ${color.b}`;
+    h2.textContent = `${color.name}`;
+    h3.textContent = `HEX: ${color.hex}, RGB: ${color.r} - ${color.g} - ${color.b}`;
   });
-  
-colorInfo.appendChild(h3);
-root.appendChild(colorInfo);
+
+root.appendChild(h2);
+root.appendChild(h3);
